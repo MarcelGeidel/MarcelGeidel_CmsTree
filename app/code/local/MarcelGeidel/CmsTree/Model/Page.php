eb2fe05f55d13ef77640a7980198cfc9c70deaf1
Marcel Geidel <marcel.geidel@retol.de>
2019-10-21 12:10:05 +0200

    Ein paar neue Hilfsfunktionen

--- app/code/local/MarcelGeidel/CmsTree/Model/Page.php
+++ app/code/local/MarcelGeidel/CmsTree/Model/Page.php
@@ -17,11 +17,56 @@ class MarcelGeidel_CmsTree_Model_Page extends Mage_Cms_Model_Page
 		{
 			$parentPage = Mage::getModel('marcelgeidel_cmstree/page')->load($parentID);
 			
-			$parents[] = $parentPage;
+			$parents = array($parentPage);
 			
 			return array_merge($parents, $parentPage->getParents($storeID));
 		}
 		
+		return array();
+	}
+	
+	public function getRootParent($storeID)
+	{
+		$parents = $this->getParents($storeID);
+		
+		if ($parents)
+		{
+			return end($parents);
+		}
+		
+		return $this;
+	}
+	
+	public function getParent($storeID)
+	{
+	    $parents = $this->getParents($storeID);
+	    
+	    if ($parents)
+	    {
+	        return reset($parents);
+	    }
+	    
+	    return $this;
+	}
+	
+	public function getChildren($storeID)
+	{
+		$query = 'SELECT page_id FROM marcelgeidel_cmstree WHERE parent_id = :parent_id and store_id = :store_id';
+		$binds = array('parent_id' => $this->getId(), 'store_id' => $storeID);
+		$result = $this->getReader()->query($query, $binds);
+	
+		if ($result)
+		{
+		    $children = array();
+		    
+			while ($row = $result->fetch())
+			{
+				$children[] = Mage::getModel('marcelgeidel_cmstree/page')->load($row['page_id']);
+			}
+			
+			return $children;
+		}
+	
 		return array();
 	}
 }
